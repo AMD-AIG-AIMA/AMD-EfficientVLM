61de1775a13b558ca0cb2db6f00bcea50f054103
zhenhliu <zhenhliu@amd.com>
2025-06-20 22:07:33 +0800

    Add files via upload

--- llava/model/multimodal_encoder/builder.py
+++ llava/model/multimodal_encoder/builder.py
@@ -1,1 +1,14 @@
+import os
+from .clip_encoder import CLIPVisionTower
+from .siglip_encoder import SigLipVisionTower
 
+def build_vision_tower(vision_tower_cfg, **kwargs):
+    vision_tower = getattr(vision_tower_cfg, 'mm_vision_tower', getattr(vision_tower_cfg, 'vision_tower', None))
+    is_absolute_path_exists = os.path.exists(vision_tower)
+    if "siglip" in vision_tower:
+        return SigLipVisionTower(vision_tower, vision_tower_cfg=vision_tower_cfg, **kwargs)
+    elif is_absolute_path_exists or vision_tower.startswith("openai") or vision_tower.startswith("laion"):
+        return CLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
+    
+
+    raise ValueError(f'Unknown vision tower: {vision_tower}')
